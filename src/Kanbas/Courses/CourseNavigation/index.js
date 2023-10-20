0709200ae7d332d924a4f3fd8aced3d0e3faadd0
Michelle-MingxuanWang <mingxuan.wang@outlook.com>
2023-10-19 19:59:34 -0700

    fix style

--- src/Kanbas/Courses/CourseNavigation/index.js
+++ src/Kanbas/Courses/CourseNavigation/index.js
@@ -2,18 +2,22 @@ import { Link, useParams, useLocation } from "react-router-dom";
 import "./index.css";
 
 function CourseNavigation() {
-  const links = ["Home", "Modules", "Piazza", "Zoom Meetings", "Assignments", "Quizzes", 
-  "Grades", "Panopto Video", "Discussions", "Announcements", "Pages", "Settings", 
-  "Files", "Rubrics", "Outcomes", "Collaborations", "Syllabus"];
+  const links = [
+    "Home", "Modules", "Piazza", "ZoomMeetings", "Assignments", "Quizzes",
+    "Grades", "PanoptoVideo", "Discussions", "Announcements", "Pages", "Settings",
+    "Files", "Rubrics", "Outcomes", "Collaborations", "Syllabus"
+  ];
+
   const { courseId } = useParams();
   const { pathname } = useLocation();
+
   return (
-    <div className="wd-course-navigation list-group" style={{ width: 100 }}>
+    <div className="wd-course-navigation list-group">
       {links.map((link, index) => (
         <Link
           key={index}
           to={`/Kanbas/Courses/${courseId}/${link}`}
-          className={`list-group-item ${pathname.includes(link) && "active"}`}
+          className={`list-group-item ${pathname === `/Kanbas/Courses/${courseId}/${link}` ? "active" : ""}`}
         >
           {link}
         </Link>
