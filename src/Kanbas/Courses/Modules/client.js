83fab98d04983751d748ac6d86e35165b4b42eca
Michelle-MingxuanWang <mingxuan.wang@outlook.com>
2023-11-20 20:58:50 -0800

    add url

--- src/Kanbas/Courses/Modules/client.js
+++ src/Kanbas/Courses/Modules/client.js
@@ -2,9 +2,14 @@ import axios from "axios";
 
 // const COURSES_URL = "http://localhost:4000/api/courses";
 // const MODULES_URL = "http://localhost:4000/api/modules";
-const API_BASE = process.env.REACT_APP_API_BASE || 'http://localhost:4000/api';
-const MODULES_URL = `${API_BASE}/modules`;
-const COURSES_URL = `${API_BASE}/courses`;
+//const API_BASE = process.env.REACT_APP_API_BASE || 'http://localhost:4000/api';
+
+//const MODULES_URL = `${API_BASE}/modules`;
+//const COURSES_URL = `${API_BASE}/courses`;
+
+const COURSES_URL = "https://dashboard.heroku.com/apps/kanbas-node-server-app-michell/deploy/github/courses";
+const MODULES_URL = "https://dashboard.heroku.com/apps/kanbas-node-server-app-michell/deploy/github/modules";
+
 
 export const addModule = async (courseId, module) => {
     const response = await axios.post(
